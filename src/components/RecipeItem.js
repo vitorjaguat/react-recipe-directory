292db497d093168a752aebde687a0b227da13af5
Vitor Jaguat <vitorbutkus@gmail.com>
2022-12-20 20:18:08 -0300

    create RecipeItem component and refactor

---
+++ src/components/RecipeItem.js
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import { useTheme } from '../hooks/useTheme';
+import Trashcan from '../assets/trashcan.svg';
+import { projectFirestore } from '../firebase/config';
+import './RecipeItem.css';
+
+export default function RecipeItem({ recipe }) {
+  const { mode } = useTheme();
+
+  const handleClick = (id) => {
+    projectFirestore.collection('recipes').doc(id).delete();
+  };
+
+  return (
+    <div key={recipe.id} className="recipe-item">
+      <div className={`card ${mode}`}>
+        <div className="info">
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make.</p>
+          <div>{recipe.method.substring(0, 400)}...</div>
+          <div className="links">
+            <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+            <img
+              className="delete"
+              src={Trashcan}
+              alt="delete"
+              onClick={() => handleClick(recipe.id)}
+            />
+          </div>
+        </div>
+        <div className="img">
+          <img src={recipe.image} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,42 +1,16 @@
-import { Link } from 'react-router-dom';
-import { useTheme } from '../hooks/useTheme';
-import Trashcan from '../assets/trashcan.svg';
-import { projectFirestore } from '../firebase/config';
-
 //styles
 import './RecipeList.css';
+import RecipeItem from './RecipeItem';
 
 export default function RecipeList({ recipes }) {
-  const { mode } = useTheme();
-
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>;
   }
 
-  const handleClick = (id) => {
-    projectFirestore.collection('recipes').doc(id).delete();
-  };
-
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className={`card ${mode}`}>
-          <div className="info">
-            <h3>{recipe.title}</h3>
-            <p>{recipe.cookingTime} to make.</p>
-            <div>{recipe.method.substring(0, 400)}...</div>
-            <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
-            <img
-              className="delete"
-              src={Trashcan}
-              alt="delete"
-              onClick={() => handleClick(recipe.id)}
-            />
-          </div>
-          <div className="img">
-            <img src={recipe.image} />
-          </div>
-        </div>
+        <RecipeItem recipe={recipe} />
       ))}
     </div>
   );
