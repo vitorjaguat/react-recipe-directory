ed217d2f1c9470eeebd52be904d9d9b33a240144
Vitor Jaguat <vitorbutkus@gmail.com>
2022-12-20 17:12:03 -0300

    begin adding images, adjust styles

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -21,10 +21,15 @@ export default function RecipeList({ recipes }) {
     <div className="recipe-list">
       {recipes.map((recipe) => (
         <div key={recipe.id} className={`card ${mode}`}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.cookingTime} to make.</p>
-          <div>{recipe.method.substring(0, 100)}...</div>
-          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <div className="info">
+            <h3>{recipe.title}</h3>
+            <p>{recipe.cookingTime} to make.</p>
+            <div>{recipe.method.substring(0, 100)}...</div>
+            <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          </div>
+          <div className="img">
+            <img src="https://media-cdn.tripadvisor.com/media/photo-s/1c/2f/33/2d/healthy-bowl-frische.jpg" />
+          </div>
           <img
             className="delete"
             src={Trashcan}
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,23 +1,56 @@
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import RecipeList from '../../components/RecipeList';
-import { useFetch } from '../../hooks/useFetch';
 import './Search.css';
+import { projectFirestore as db } from '../../firebase/config';
+import loader from '../../assets/loading.svg';
+import { useTheme } from '../../hooks/useTheme';
 
 export default function Search() {
   const queryString = useLocation().search;
   const queryParams = new URLSearchParams(queryString);
   const query = queryParams.get('q');
-  console.log(query);
 
-  const url = 'http://localhost:3000/recipes?q=' + query;
-  const { error, isPending, data } = useFetch(url);
+  const { mode } = useTheme();
+
+  const [recipes, setRecipes] = useState(null);
+  const [error, setError] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+    setRecipes(null);
+    const collectionRef = db.collection('recipes');
+    collectionRef.get().then((docs) => {
+      let docArray = [];
+      docs.forEach((doc) => {
+        if (doc.data().title.toLowerCase().includes(query.toLowerCase())) {
+          docArray.push({ id: doc.id, ...doc.data() });
+        }
+      });
+      setIsPending(false);
+      setRecipes(docArray);
+    });
+  }, [query]);
 
   return (
     <div>
-      <h2 className="page-title">Recipes including "{query}"</h2>
-      {error && <p className="error">{error}</p>}
-      {isPending && <p className="loading">Loading...</p>}
-      {data && <RecipeList recipes={data} />}
+      <h1 className={`page-title ${mode}`}>Search results for "{query}"</h1>
+      {error && <p className={`error ${mode}`}>{error}</p>}
+      {isPending && (
+        <div className="loader-image">
+          <img
+            src={loader}
+            alt="Loading"
+            style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+          />
+        </div>
+      )}
+      {recipes && (
+        <div className="search-results">
+          <RecipeList recipes={recipes} />
+        </div>
+      )}
     </div>
   );
 }
