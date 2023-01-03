ae2c4d85207af4a84b3ee0a239a8e4153ca64d14
Vitor Jaguat <vitorbutkus@gmail.com>
2023-01-03 15:01:21 -0300

    add logout btn in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import { useTheme } from '../hooks/useTheme';
 import AuthContext from '../context/AuthContext';
+import logout from '../assets/logout.svg';
 
 //styles
 import './Navbar.css';
 
 export default function Navbar() {
-  const { color } = useTheme();
+  const { color, mode } = useTheme();
   const authCtx = useContext(AuthContext);
 
   return (
@@ -18,8 +19,29 @@ export default function Navbar() {
           <h1>Appetite</h1>
         </Link>
         <SearchBar />
-        {authCtx.isLoggedIn && <Link to="/create">Create Recipe</Link>}
-        {!authCtx.isLoggedIn && <Link to="/login">Signup/Login</Link>}
+        {authCtx.isLoggedIn && (
+          <Link className="link" to="/create">
+            Create Recipe
+          </Link>
+        )}
+        {!authCtx.isLoggedIn && (
+          <Link className="link" to="/login">
+            Signup/Login
+          </Link>
+        )}
+
+        {authCtx.isLoggedIn && (
+          <div className="logout">
+            <img
+              src={logout}
+              alt="Logout"
+              onClick={authCtx.logout}
+              style={{
+                filter: 'invert(100%',
+              }}
+            />
+          </div>
+        )}
       </nav>
     </div>
   );
